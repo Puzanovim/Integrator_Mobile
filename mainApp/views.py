eb6fd61cefab2ffed6fcc6aead75164db27feece
Puzanovim <puzanovim@yandex.ru>
2019-10-20 12:52:00 +0500

    first commit

---
+++ mainApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'mainApp/index.html')
+
+def contact(request):
+    return render(request, 'mainApp/wrapper.html')
